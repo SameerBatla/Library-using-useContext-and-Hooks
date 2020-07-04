8ded0f68f6c91514709013c615236dd9edbb3732
SameerBatla <sambatla456@gmail.com>
2020-07-04 17:42:03 +0530

    set up the basic structure

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Booklist extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>The Wings of Fire</li>
+          <li>Percy Jackson</li>
+          <li>Binding Show</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
